43963987a70aea2ae157d948ed7e35dd24cf87b5
AbhiGone <giriabhinaya@gmail.com>
2023-09-15 09:09:13 -0400

    part 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    double price;
+    double salesTax = .05;
+
+    System.out.println("Enter the price:");
+    price = scan.nextDouble();
+
+    double tax = price * salesTax;
+    double total = tax + price;
+    System.out.println("Your total after the tax is, " + total);
+
     }
 }
